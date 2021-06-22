059edc392f35376c63b0d6bfed000bf4d6547e6b
HallvardMM <hallvardmolinmorstol@gmail.com>
2021-06-22 19:33:45 +0200

    Proglab 2 filer

---
+++ Prosjekt1/Morse Decoder/Arduino_morse/Arduino_morse.ino
@@ -0,0 +1,112 @@
+const int ledGreen = 8;           // Indicates break
+const int ledYellow = 7;           // Indicates dot
+const int ledRed = 6;             // Indicates dash
+const int buttonPin = 2;          // button
+
+// Variables will change:
+int buttonState;             // the current reading from the input pin
+int lastButtonState = HIGH;   // the previous reading from the input pin
+
+int startSym = 0;
+int endSym = 0;
+int pauseTime = 0;
+int message = 0;
+int pressedTime = 0;
+
+int T = 500;
+
+//-----------------SETUP---------------------------------------
+
+void setup(){
+  pinMode( ledGreen, OUTPUT );    // Pin 8 is an output
+  pinMode( ledYellow, OUTPUT );    // Pin 7 is an output
+  pinMode( ledRed, OUTPUT );    // Pin 6 is an output
+  pinMode( buttonPin, INPUT);   // Takes in values from button
+  Serial.begin(9600);            // Important for port output
+
+  endSym = millis();
+  
+  digitalWrite(ledGreen, HIGH); // set initial LED state
+  digitalWrite(ledYellow, LOW); // set initial LED state
+  digitalWrite(ledRed, LOW); // set initial LED state
+}
+
+
+//--------------------- morse--------------------------
+
+void morse(){
+  if (buttonState == LOW){
+
+    startSym = millis();
+    pauseTime = startSym - endSym;
+
+    
+    //digitalWrite(ledGreen, HIGH);
+    //digitalWrite(ledYellow, LOW);
+    //digitalWrite(ledRed, LOW);
+    
+    //Short pause brukes kun for å sjekke i serial overvåker.
+      if (pauseTime <= T) {
+        message = 3;
+      }
+    //Medium pause
+      if (pauseTime > T && pauseTime <= 3*T) {
+      message = 4;
+      }
+    //Long pause
+    if (pauseTime > 3*T) {
+      message = 5;
+    }
+    
+  }
+
+  else{
+    digitalWrite(ledGreen, LOW);
+
+    endSym = millis();
+    pressedTime = endSym - startSym;
+
+    //Short press - dot
+    if(pressedTime<=0.6*T){
+      digitalWrite(ledGreen, LOW);
+      digitalWrite(ledYellow, HIGH);
+      digitalWrite(ledRed, LOW);
+      delay(50);
+      
+      message = 1;
+    };
+
+    //Long press - dash
+    if(pressedTime>0.6*T){
+      digitalWrite(ledGreen, LOW);
+      digitalWrite(ledYellow, LOW);
+      digitalWrite(ledRed, HIGH);
+      delay(50);
+
+      message = 2;
+    }
+
+    //delay(20);
+
+    digitalWrite(ledGreen, HIGH);
+    digitalWrite(ledYellow, LOW);
+    digitalWrite(ledRed, LOW);
+    
+  }
+
+  Serial.print(message);
+}
+
+//--------- Loop-----------------------------------------------
+void loop(){
+ buttonState = digitalRead(buttonPin);
+
+  if (buttonState != lastButtonState) {
+    morse();
+  }
+  
+  delay(20);    //Debounce
+
+  lastButtonState = buttonState;
+  
+}
